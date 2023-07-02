fd230d9eda82532e4451956b6f02ed9518769590
DingoPham <pencibambo@gmail.com>
2023-07-02 15:23:47 +0700

    update 7/2

--- GiaoDien/content.js
+++ GiaoDien/content.js
@@ -1,19 +1,12 @@
 function toggleContent() {
     var content = document.getElementById("content");
-    var image = document.createElement("img");
-    image.src = "img/chevrons-down_20_20.png";
-    image.classList.add("angle-down");
     var button = document.getElementById("toggleButton");
-    
-    if (content.classList.contains("collapsed")) {
-      content.classList.remove("collapsed");
-      content.classList.add("expanded");
-      content.appendChild(image)
+
+    if (content.style.display === "none") {
+      content.style.display = "block" ;
       button.textContent = "Thu gọn ";
     } else {
-      content.classList.remove("expanded");
-      content.classList.add("collapsed");
-      content.appendChild(image)
+      content.style.display = "none";
       button.textContent = "Xem thêm ";
     }
   }
