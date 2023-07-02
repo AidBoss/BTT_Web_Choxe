22030bd2b455ef04358141a5edf7dfbf73cde525
Ankdev <anhdinh88ntt@gmail.com>
2023-07-02 12:43:05 +0700

    tach suon trang web

--- GiaoDien/content.js
+++ GiaoDien/content.js
@@ -1,19 +1,52 @@
-function toggleContent() {
-    var content = document.getElementById("content");
-    var image = document.createElement("img");
-    image.src = "img/chevrons-down_20_20.png";
-    image.classList.add("angle-down");
-    var button = document.getElementById("toggleButton");
-    
-    if (content.classList.contains("collapsed")) {
-      content.classList.remove("collapsed");
-      content.classList.add("expanded");
-      content.appendChild(image)
-      button.textContent = "Thu gọn ";
-    } else {
-      content.classList.remove("expanded");
-      content.classList.add("collapsed");
-      content.appendChild(image)
-      button.textContent = "Xem thêm ";
-    }
-  }
+// const formOpenBtn = document.querySelector("#form-open"),
+//   home = document.querySelector(".home"),
+//   formContainer = document.querySelector(".form_container"),
+//   formCloseBtn = document.querySelector(".form_close"),
+//   signupBtn = document.querySelector("#signup"),
+//   loginBtn = document.querySelector("#login"),
+//   pwShowHide = document.querySelectorAll(".pw_hide");
+
+// formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+// formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+// pwShowHide.forEach((icon) => {
+//   icon.addEventListener("click", () => {
+//     let getPwInput = icon.parentElement.querySelector("input");
+//     if (getPwInput.type === "password") {
+//       getPwInput.type = "text";
+//       icon.classList.replace("uil-eye-slash", "uil-eye");
+//     } else {
+//       getPwInput.type = "password";
+//       icon.classList.replace("uil-eye", "uil-eye-slash");
+//     }
+//   });
+// });
+
+// signupBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   formContainer.classList.add("active");
+// });
+// loginBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   formContainer.classList.remove("active");
+// });
+
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
