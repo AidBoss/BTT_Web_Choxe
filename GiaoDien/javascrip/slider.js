d94f305ea9cfbe51922a27000f66d7a91a2117e0
Ankdev <anhdinh88ntt@gmail.com>
2023-07-04 23:26:30 +0700

    phan silder

--- GiaoDien/javascrip/slider.js
+++ GiaoDien/javascrip/slider.js
@@ -1,10 +1,23 @@
+let list_img = document.querySelectorAll(".list-img img")
+let img_mid = document.querySelector(".img_mid img")
+// let prev = document.querySelector("#prev")
+// let next = document.querySelector("#next")
+console.log(list_img)
+console.log(img_mid)
+let index = 0;
+list_img.forEach(event => {
+    event.addEventListener('click', e => {
+        img_mid.src = e.target.getAttribute('src')
 
-
-const imgFeature = document.querySelector('.img-feature')
-const listImg = document.querySelectorAll('.list-img img')
-
-listImg.forEach((imgElement, index) => {
-    imgElement.addEventListener('click', e => {
-        updateImageByIndex(index)
     })
 });
+next = () => {
+    index++;
+    if (index == list_img.length) index = 0;
+    img_mid.src = list_img[index]
+}
+prev = () => {
+    index--;
+    if (index == list_img.length) index = 0;
+    img_mid.src = list_img[index]
+}
