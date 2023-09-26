2e6e94f8393bed035d358bb68972289eb61b023b
TuanDo33 <tuandoplt@gmail.com>
2023-09-26 22:09:08 +0700

    update nè

---
+++ GiaoDien/javascrip/dangtin.js
@@ -0,0 +1,14 @@
+function toggleShow(){
+    const bShow = document.getElementById("btnShow");
+    const bHide = document.getElementById("btnHide");
+
+    bHide.style.display = 'block';
+    bShow.style.display = 'none';
+}
+function nutShow(){
+    const bnShow = document.getElementById("btShow");
+    const bnHide = document.getElementById("btHide");
+
+    bnHide.style.display = 'none';
+   bnShow.style.display = 'block';
+}
