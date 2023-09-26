ca64758b4d9a0d57710921ee795add8409b8bcba
AidBoss <anhdinh88ntt@gmail.com>
2023-09-26 15:52:33 +0700

    upload html of User

--- GiaoDien/javascrip/content.js
+++ GiaoDien/javascrip/content.js
@@ -1,39 +1,29 @@
-const show = document.querySelector('#bt_show')
-const btshow = document.querySelector('#btu_show')
+const show = document.querySelector("#bt_show");
+const btshow = document.querySelector("#btu_show");
 let check = true;
 let eventclickshow = function () {
   if (check) {
-    btshow.style.display = 'block';
-    show.style.display = 'block';
+    btshow.style.display = "block";
+    show.style.display = "block";
     btshow.innerHTML = " Ẩn đi &uarr;";
-    btshow.style.fontSize = '15px';
-    register.style.display = 'block';
-    btchange.style.display = 'block';
-    login.style.display = 'none';
-    btchange.innerHTML = 'Đăng Nhập'
+    btshow.style.fontSize = "15px";
     check = false;
   } else {
-    btshow.innerHTML = " Xem Thêm &darr;"
-    show.style.display = 'none';
-    register.style.display = 'none';
-    login.style.display = 'block';
-    btchange.innerHTML = 'Đăng Ký'
+    btshow.innerHTML = " Xem Thêm &darr;";
+    show.style.display = "none";
     check = true;
   }
-}
-const register = document.querySelector('.register_modal')
-const btchange = document.querySelector('#change_login')
-const login = document.querySelector('.login_modal')
+};
 
-let sonam = document.querySelector(".sonam")
-let namsx = document.querySelector("#namsx")
+let sonam = document.querySelector(".sonam");
+let namsx = document.querySelector("#namsx");
 namsx.innerHTML = sonam.value;
 sonam.oninput = function () {
   namsx.innerHTML = this.value;
-}
+};
 let slider2 = document.getElementById("myRange2");
 let output2 = document.getElementById("demo2");
 output2.innerHTML = slider2.value;
 slider2.oninput = function () {
   output2.innerHTML = this.value;
-}
+};
