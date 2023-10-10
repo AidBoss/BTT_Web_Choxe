5796a2b2c28f070c644acbbec54d3af3711b567f
TuanDo33 <tuandoplt@gmail.com>
2023-10-10 23:23:42 +0700

    update ne

--- GiaoDien/config/delete_all.php
+++ GiaoDien/config/delete_all.php
@@ -5,6 +5,6 @@ if(isset($_GET['del_product']))
 {
     $delete=$getdata->delete_all('dangtin',$_GET['del_product']);
 if($delete) echo"<script>alert('bạn đã xóa thành công !') 
-window.location='../Admin/view/listProduct.php'</script>";
+window.location='../Admin/index.php?act=qlsp'</script>";
 else echo"<script>alert('bạn không xóa được')</script>";
 }
